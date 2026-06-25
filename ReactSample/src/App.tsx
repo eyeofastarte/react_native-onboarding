7d584b6aba9dfa6c33d91a74ae688d136fe47b69
Astarte <93458816+eyeofastarte@users.noreply.github.com>
2026-06-25 13:33:14 +1000

    Add tailwind and verify

--- ReactSample/src/App.tsx
+++ ReactSample/src/App.tsx
@@ -21,9 +21,10 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
+        {/* Remove counter className */}
         <button
           type="button"
-          className="counter"
+          className="cursor-pointer py-1.25 px-2.5 rounded-[5px] text-(--accent) bg-(--accent-bg) border-2 border-transparent transition-[colors,scale] duration-300 mb-6 hover:border-(--accent-border) focus-visible:outline-2 focus-visible:outline-(--accent) focus-visible:outline-offset-2 tracking-wider active:scale-95"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
